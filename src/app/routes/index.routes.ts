b6bcfc9e5adbf2504038fb9793b91eda1a3e7864
jmamadeu <jmamadeu2000@gmail.com>
2020-05-03 11:41:59 +0100

    :art:  feat: Removing desnecessary files

--- src/app/routes/index.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import DiaseaseTypesRoutes from './DiaseaseType.routes'
-
-const routes = express.Router()
-
-routes.use('/diasease-types', DiaseaseTypesRoutes)
-
-export default routes
