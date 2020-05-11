7938ac65930394369a3e7ebf067769194fc96f8f
jmamadeu <jmamadeu2000@gmail.com>
2020-05-11 14:07:24 +0100

    :zap: [feat]: Creating routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,5 +1,26 @@
 import express from 'express';
 
+import SymptomController from '../controllers/SymptomController';
+import CategoryController from '../controllers/CategoryController';
+import SubCategoryController from '../controllers/SubCategoryController';
+import ClassificationController from '../controllers/ClassificationController';
+import DiseaseController from '../controllers/DiseaseController';
+
 const routes = express.Router();
+
+routes.get('/categories', CategoryController.index);
+routes.post('/categories', CategoryController.store);
+
+routes.get('/subcategories', SubCategoryController.index);
+routes.post('/subcategories', SubCategoryController.store);
+
+routes.get('/symptoms', SymptomController.index);
+routes.post('/symptoms', SymptomController.store);
+
+routes.get('/classifications', ClassificationController.index);
+routes.post('/classifications', ClassificationController.store);
+
+routes.get('/diseases', DiseaseController.index);
+routes.post('/diseases', DiseaseController.store);
 
 export default routes;
